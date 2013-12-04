10d3f5fba34f25ea4d21d0fa3bdc4d03c07f7543
javier gago <jgagoromero@gmail.com>
2013-12-04 08:57:35 +0100

    release 1.0

--- js/FelizNavidad.js
+++ js/FelizNavidad.js
@@ -3,10 +3,15 @@ var ctx = null;
 var personaje=null;
 var personaje2=null;
 var papaNoel=null;
+var reno=null;
 var arbol=null;
+var regalos=null;
+var regalosAbiertos=null;
 var arrayNieve;
 var EM;
 var mensaje;
+var estado;
+var parpadeo;
 
 
 function setFullScreen(){
@@ -53,17 +58,29 @@ main = function() {
 	canvas.height = window.innerHeight;
 	setFullScreen();
 	
-	personaje=new PNJ(0,canvas.height/2-32,2,2,0,canvas.width,"imagenes/person-sprite.png");
-	personaje2=new PNJ(canvas.width-32,canvas.height/2-32,2,2,0,canvas.width,"imagenes/person-sprite2.png");
-	papaNoel=new PNJ(0,0,6,2,0,canvas.width,"imagenes/reno-sprite.png");
+	estado=0;
+	parpadeo=0;
+	
+	personaje=new PNJ(0,canvas.height/2-20,2,2,0,canvas.width,"imagenes/person-sprite.png");
+	personaje2=new PNJ(canvas.width-32,canvas.height/2-20,2,2,0,canvas.width,"imagenes/person-sprite2.png");
+	papaNoel=new PNJ(-64,canvas.height/2-10,2,5,-128,canvas.width+128,"imagenes/noel-sprite.png");
+	reno=new PNJ(-128,canvas.height/2-10,2,5,-128,canvas.width+128,"imagenes/reno-sprite.png");
+	
 	arrayNieve=new Nieve(200);
+	
 	arbol=new Image();
 	arbol.src="imagenes/christmas-tree.png";
+	
+	regalos=new Image();
+	regalos.src="imagenes/regalos.png";
+	
+	regalosAbiertos=new Image();
+	regalosAbiertos.src="imagenes/regalos-abiertos.png";
 	
 	EM=new EventManager();
 	EM.canvas = canvas;
 	EM.addEventsListeners();
-	mensaje=new TextoAnimado(40,"Lucida Handwriting","¡FELIZ NAVIDAD "+locationVars('nombre')+"!",0,1);
+	mensaje=new TextoAnimado(40,"Arial","¡FELIZ NAVIDAD "+locationVars('nombre')+"!",0,3,"white");
 	setInterval(hilo_juego,100);
 }
 
@@ -79,32 +96,68 @@ var render_juego=function(){
 	
 	ctx.fillStyle="white";
 	ctx.fillRect(0,canvas.height/2,canvas.width,canvas.height/2);
-	
+		
+	ctx.drawImage(arbol,canvas.width/2-64,canvas.height/2-118);
+
 	personaje.render();
 	personaje2.render();
 	papaNoel.render();
+	reno.render();
 	
-	ctx.drawImage(arbol,canvas.width/2-64,canvas.height/2-118);
-			
-	ctx.font="italic 20px Lucida Handwriting";
+	if(estado===2)
+	{
+		ctx.drawImage(regalos,canvas.width/2-41,canvas.height/2+5);
+	}else if(estado>=3){
+		ctx.drawImage(regalosAbiertos,canvas.width/2-41,canvas.height/2+5);
+		ctx.font="bold 10px Arial";
+		if(parpadeo>=5){
+			ctx.fillStyle="magenta";
+		}else{
+			ctx.fillStyle="red";
+		}
+		ctx.textAlign="center";
+		ctx.textBaseline="bottom";
+		ctx.fillText("¡FELICIDADES "+locationVars('nombre')+"!",canvas.width/2,3*canvas.height/4,canvas.width);
+	}
+	
+	ctx.font="italic 20px Arial";
 	ctx.fillStyle="green";
 	ctx.textAlign="center";
 	ctx.textBaseline="bottom";
-	ctx.fillText("By J.GAGO",canvas.width/2,3*canvas.height/4,canvas.width);
+	ctx.fillText("By J.GAGO",canvas.width/2,canvas.height,canvas.width);
 	
 	arrayNieve.render();
 }
 
 var logica_juego=function(){
 	if(EM.isTouchUp()) {
-		if(personaje.tocado(EM.canX,EM.canY)===1){
+		if((personaje.tocado(EM.canX,EM.canY)===1) && (personaje.direccion===2)){
 			personaje.mover(6,2,0,canvas.width);
 		}
 		
-		if(personaje2.tocado(EM.canX,EM.canY)===1){
+		if((personaje2.tocado(EM.canX,EM.canY)===1)&&(personaje2.direccion===2)){
 			personaje2.mover(3,2,0,canvas.width);
 		}
-	}	
+		
+		if((estado===1)&&(EM.canX>=canvas.width/2-64)&&(EM.canX<=canvas.width/2+64)&&(EM.canY>=canvas.height/2-118)&&(EM.canY<=canvas.height/2-2)){
+			estado=2;
+		}
+		
+		if((estado===2)&&(EM.canX>=canvas.width/2-41)&&(EM.canX<=canvas.width/2+41)&&(EM.canY>=canvas.height/2+5)&&(EM.canY<=canvas.height/2+35)){
+			estado=3;
+			papaNoel.mover(6,5,-128,canvas.width+128);
+			reno.mover(6,5,-128,canvas.width+128);
+		}
+	}
+	
+	if((estado===0)&&(personaje.direccion!=2)&&(personaje2.direccion!=2)){
+		estado=1;
+	}
+	
+	if(estado>=3){
+		parpadeo++;
+		parpadeo=parpadeo%10;
+	}
 }
 
 window.onload = main;
--- js/TextoAnimado.js
+++ js/TextoAnimado.js
@@ -1,13 +1,14 @@
 var canvas = null;
 var ctx = null;
 
-var TextoAnimado=function(tam,fuente,cadena,pos,velocidad){
+var TextoAnimado=function(tam,fuente,cadena,pos,velocidad,color){
 	this.tam=tam;
 	this.fuente=fuente;
 	this.contador;
 	this.velocidad=velocidad;
 	this.posicion=pos;
 	this.cadenaMensaje=cadena;
+	this.color=color;
 	
 	canvas  = document.getElementById('canvas');
 
@@ -23,7 +24,7 @@ var TextoAnimado=function(tam,fuente,cadena,pos,velocidad){
 	
 	this.render=function(){
 		ctx.font="bold "+this.tam+"px "+this.fuente;
-		ctx.fillStyle="white";
+		ctx.fillStyle=this.color;
 		ctx.textAlign="left";
 		ctx.textBaseline="top";
 		ctx.fillText(this.cadenaMensaje,this.contador,this.posicion);
